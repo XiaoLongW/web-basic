602ca1af39ca6aad8486baf44e3b1547ce60d159
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-13 21:29:52 +0800

    change index-controll.js

--- controll/index-controll.js
+++ controll/index-controll.js
@@ -1,13 +1,24 @@
+var TopicHelper = require('../helper/topic-helper.js');
+var page = require('../page.json')
 function Controller(){
 }
 
-Controller.prototype.indexPost = function (req,res){
-  res.render('index');
+Controller.prototype.indexGet = function (req,res){
+  var questions = page.questions;
+  var topicHelper = new TopicHelper();
+  var topics = topicHelper.getTopics(questions, req);
+  var score = '';
+  res.render('index', {'topic':topics,'scores':score});
 }
 
-Controller.prototype.indexGet = function (req,res){
-  res.render('index');
-  console.log(req.body);
+Controller.prototype.indexPost = function (req,res){
+  var questions = page.questions;
+  var topicHelper = new TopicHelper();
+  var topics = topicHelper.getTopics(questions, req);
+  var score = topicHelper.getTotalScore(topics);
+  res.render('index', {'topic':topics,'scores':score});
 }
+
+
 
 module.exports = Controller;
