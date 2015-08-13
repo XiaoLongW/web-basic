7cb81018d8370e836254e6ebbadd69fa47efaa7a
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-13 13:28:36 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function() {
+  console.log('listening port 3000');
+});
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+module.exports = app;
