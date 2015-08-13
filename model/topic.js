94e22d9a12b2ebff901bdf67066c37359f0b5db9
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-13 15:05:59 +0800

    add topic.js

---
+++ model/topic.js
@@ -0,0 +1,12 @@
+function Topic(question,req){
+  this.title = question.topic;
+  this.option = question.choices || [];
+  this.answer = question.answer;
+  this.score = question.score;
+  this.inputs = req.body[question.name] || [];
+}
+
+Topic.prototype.getScore = function(){
+}
+
+module.exports = Topic;
