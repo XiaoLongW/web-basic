02ccffd3b6c3878cc73d61833c6f3ddca0c45ab3
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-08-18 23:44:53 +0800

    change index-controll.js

--- controll/index-controll.js
+++ controll/index-controll.js
@@ -1,24 +1,25 @@
 'use strict';
 var TopicHelper = require('../helper/topic-helper');
 var HomeViewModel = require('../view-model/home-view-model')
-var page = require('../seeds/page.json')
+var ConnectSql = require('../seeds/get-sql')
 
 function Controller(){
 }
 
+var connectSql = new ConnectSql();
+connectSql.getData();
+
 Controller.prototype.indexGet = function (req,res){
-  var questions = page.questions;
   var topicHelper = new TopicHelper();
-  var topics = topicHelper.getTopics(questions);
+  var topics = topicHelper.getTopics(connectSql.data);
   topicHelper.setInputs(topics, req.body);
   var homeViewModel = new HomeViewModel(topics,req.body);
   res.render('index', homeViewModel);
 }
 
 Controller.prototype.indexPost = function (req,res){
-  var questions = page.questions;
   var topicHelper = new TopicHelper();
-  var topics = topicHelper.getTopics(questions);
+  var topics = topicHelper.getTopics(connectSql.data);
   topicHelper.setInputs(topics, req.body);
   var homeViewModel = new HomeViewModel(topics,req.body);
   res.render('index', homeViewModel);
